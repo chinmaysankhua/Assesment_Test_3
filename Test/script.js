5a6fa74d1591855d07373348f897a2977ea16aeb
Chinmay <gunk2105@gmail.com>
2025-03-14 14:58:26 +0530

    first commit

---
+++ Test/script.js
@@ -0,0 +1,6 @@
+import { createClient } from 'pexels';
+
+const client = createClient('LUZBAoKoryv3rhUL0YwYxyYOhDspkIM97iUGW0cC2oT5erOKRFCrhGEs');
+
+// All requests made with the client will be authenticated
+
